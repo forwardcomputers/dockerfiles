1abd184dbfcfa3f717c8057b646f5daed051e93b
Ali Mustakim <alim@forwardcomputers.com>
2019-03-22 14:34:40 -0400

    updates

--- dserver/entrypoint.sh
+++ dserver/entrypoint.sh
@@ -9,5 +9,5 @@ rm -rf tmp/ .profile
 git reset --hard
 git remote set-url origin ssh://git@github.com/forwardcomputers/dfiles.git
 #
-sudo cp /etc/hosts ~/hosts.new && sed "s/^127.0.0.1.*/127.0.0.1\tlocalshost "$(cat /etc/hostname)"/" -i ~/hosts.new && cp -f ~/hosts.new /etc/hosts
-sudo /usr/sbin/sshd -D
+sudo bash -c 'cp /etc/hosts ~/hosts.new; sed "s/^127.0.0.1.*/127.0.0.1\tlocalhost "$(cat /etc/hostname)"/" -i ~/hosts.new; cp -f ~/hosts.new /etc/hosts'
+sudo bash -c '/usr/sbin/sshd -D'
