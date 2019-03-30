a5ff075ac727e48353f2c92caf753e3f58d5bb72
Ali Mustakim <alim@forwardcomputers.com>
2019-03-30 09:21:35 -0400

    updates [skip ci]

--- dserver/entrypoint.sh
+++ dserver/entrypoint.sh
@@ -5,7 +5,6 @@ if [[ ! -d "${HOME}"/.git ]]; then
     cd "${HOME}"
     git clone --recursive https://github.com/forwardcomputers/dfiles.git tmp > /dev/null 2>&1
     mv tmp/.git .
-    rm -rf tmp
     rm -rf tmp/ .profile
     git reset --hard  > /dev/null 2>&1
     git remote set-url origin ssh://git@github.com/forwardcomputers/dfiles.git > /dev/null 2>&1
