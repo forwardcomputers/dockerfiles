d20ac55612c1ee8eb4825b2a5c456bd38e50095f
Ali Mustakim <alim@forwardcomputers.com>
2020-04-22 23:14:23 -0400

    updates

---
+++ webcode/root/bin/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+#
+DOCKER_HOST_GID="$(stat -c %g /var/run/docker.sock)"
+DOCKER_CONTAINER_GID="$(awk -F':' '/docker/ {print $3}' < /etc/group)"
+if [[ "${DOCKER_HOST_GID}" != "${DOCKER_CONTAINER_GID}" ]]; then
+    sudo sed --in-place --expression='s/'"${DOCKER_CONTAINER_GID}"'/'"${DOCKER_HOST_GID}"'/' /etc/group
+fi
+if [[ ! -d "${HOME}"/.git ]]; then
+    cd "${HOME}"
+    git clone --recursive https://github.com/forwardcomputers/dfiles.git tmp > /dev/null 2>&1
+    mv tmp/.git .
+    rm -rf tmp/ .profile
+    git reset --hard  > /dev/null 2>&1
+    git remote set-url origin ssh://git@github.com/forwardcomputers/dfiles.git > /dev/null 2>&1
+fi
+#
+sudo bash -c '/init &'
+sudo bash -c '/usr/sbin/sshd -De'
