c65d9f8317bbf8d9c18452b47643bcec19e23796
Ali Mustakim <alim@forwardcomputers.com>
2020-04-25 11:44:53 -0400

    updates [skip ci]

--- webcode/root/etc/entrypoint.sh
+++ webcode/root/etc/entrypoint.sh
@@ -4,29 +4,31 @@ set -e
 DOCKER_HOST_GID="$(stat -c %g /var/run/docker.sock)"
 DOCKER_CONTAINER_GID="$(awk -F':' '/docker/ {print $3}' < /etc/group)"
 if [[ "${DOCKER_HOST_GID}" != "${DOCKER_CONTAINER_GID}" ]]; then
-    sudo sed -ie 's/'"${DOCKER_CONTAINER_GID}"'/'"${DOCKER_HOST_GID}"'/' /etc/group
+    sudo sed -ie 's/'"${DOCKER_CONTAINER_GID}"'/'"${DOCKER_HOST_GID}"'/' /etc/group 2>/dev/null
 fi
-if [[ ! -d "${HOME}"/.git ]]; then
-    cd "${HOME}"
-    git clone --recursive https://github.com/forwardcomputers/dfiles.git tmp > /dev/null 2>&1
+if [[ ! -d /home/"${user}"/.git ]]; then
+    cd /home/"${user}"
+    git clone -q --recursive https://github.com/forwardcomputers/dfiles.git tmp
     mv tmp/.git .
     rm -rf tmp/ .profile
-    git reset --hard  > /dev/null 2>&1
-    git remote set-url origin ssh://git@github.com/forwardcomputers/dfiles.git > /dev/null 2>&1
+    git reset -q --hard
+    git remote set-url origin ssh://git@github.com/forwardcomputers/dfiles.git
+    chown -R "${uid}":"${gid}" /home/"${user}"
 fi
 #
-/opt/code-server/code-server \
-  --auth none \
-  --cert /opt/filer/os/acme/webcode.home.mustakim.com/webcode.home.mustakim.com.cer \
-  --cert-key /opt/filer/os/acme/webcode.home.mustakim.com/webcode.home.mustakim.com.key \
-  --disable-ssh \
-  --disable-telemetry \
-  --disable-updates \
-  --extensions-dir /home/duser/.code-server/extensions/ \
-  --extra-builtin-extensions-dir /opt/code-extensions/ \
-  --host 0.0.0.0 \
-  --port 8080 \
-  --user-data-dir /home/duser/.code-server/data \
-  /home/duser &
+setpriv --reuid="${uid}" --regid="${gid}" --clear-groups \
+  /opt/code-server/code-server \
+    --auth none \
+    --cert /opt/filer/os/acme/webcode.home.mustakim.com/webcode.home.mustakim.com.cer \
+    --cert-key /opt/filer/os/acme/webcode.home.mustakim.com/webcode.home.mustakim.com.key \
+    --disable-ssh \
+    --disable-telemetry \
+    --disable-updates \
+    --extensions-dir /home/duser/.code-server/extensions/ \
+    --extra-builtin-extensions-dir /opt/code-extensions/ \
+    --host 0.0.0.0 \
+    --port 8080 \
+    --user-data-dir /home/duser/.code-server/data \
+    /home/duser &
 #
-sudo bash -c '/usr/sbin/sshd -De'
+bash -c '/usr/sbin/sshd -De'
